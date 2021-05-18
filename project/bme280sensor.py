add5ec0a74fcfc9eefdecd4ef32779769f93eb76
Sebastiss <56909712+Sebastiss@users.noreply.github.com>
2021-05-18 15:51:01 +0200

    New version of BME280 script
    New version of script includes connection to data base and saveing data in real time.

--- project/bme280sensor.py
+++ project/bme280sensor.py
@@ -1,16 +1,18 @@
-try:
-    from smbus2 import SMBus
-except ImportError:
-    from smbus import SMBus
+#Bosh BME280 read data form sensor - test example
+from smbus import SMBus
 from bme280 import BME280
 from time import sleep
+import mariadb
 
+# initialize the BME280 sensor
 port = 1
-#initialize the sensor
-bus = SMBus(port)
-bme280 = BME280(i2c_dev=bus)
+bus = SMBus(port) # set up the device on port number 1
+bme280 = BME280(i2c_dev=bus) # initialize BME sensor
 
-# set time in seconds bettwen measurements
+# to print degree symbol in output
+degree_sign = chr(176)#u"\N{DEGREE SIGN}"
+
+# set the value to wait defined amount of seconds bettwen next measurements
 wait = 10
 
 # calibrate the sensor
@@ -19,9 +21,15 @@ pressure = bme280.get_pressure()
 humidity = bme280.get_humidity()
 sleep(3)
 
+# initialize the connection to MariaDB Server
+db = mariadb.connect(host="localhost",user="admin", passwd="admin",db="weather_station")
+cur = db.cursor()
+
 while True:
-    temperature = bme280.get_temperature()
-    pressure = bme280.get_pressure()
-    humidity = bme280.get_humidity()
-    print(temperature, pressure, humidity)
+    ambient_temperature = round(bme280.get_temperature(), 2)
+    pressure = round(bme280.get_pressure(), 2)
+    humidity = round(bme280.get_humidity(), 2)
+    print("{:05.2f}{}C {:05.2f}hPa {:05.2f}%".format(ambient_temperature, degree_sign, pressure, humidity))
+    cur.execute('''INSERT INTO bme280_data(ambient_temperature, pressure, humidity) VALUES(%s,%s,%s);''',(ambient_temperature,pressure,humidity))
+    db.commit()
     sleep(wait)
