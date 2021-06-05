2d5b61d33f7b0d6919ad184a3ce373d877fe51e2
Sebastiss <56909712+Sebastiss@users.noreply.github.com>
2021-06-05 23:26:45 +0200

    Necessary bash scripts for runing this project
    Scripts for make project simpler in ussage

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+cd $HOME/Documents/weather_station
+
+#python3 bme280sensor.py
+python3 camera.py &
+#python3 ds18b20.py
+
+exit 0
---
+++ scripts/weather_station.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $HOME
+
+cd $HOME
+sudo apt-get update
+sudo apt-get upgrade -y
+
+mkdir $HOME/Documents/weather_station
