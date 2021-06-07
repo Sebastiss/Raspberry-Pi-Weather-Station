65d02b1d0fcc43d15b58ed6d6d26f3c75ae17b59
Sebastiss <56909712+Sebastiss@users.noreply.github.com>
2021-06-07 22:26:45 +0200

    Start and stop the project by one script
    New start stop scripts
    stop script use killall python3 method rather than old one grep

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+killall python3
 #pgrep -lf bme280sensor..py
 #pkill bme280sensor..py
 
-pgrep -lf camera..py
-pkill camera..py
+#pgrep -lf camera..py
+#pkill camera..py
 
 #pgrep -lf ds18b20..py
 #pkill ds18b20..py
